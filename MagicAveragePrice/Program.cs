d0f727728c863e72c5741a27cab23889179f8579
thegreatpl <imperialjustinian@gmail.com>
2019-07-21 18:48:01 +0100

    completed program working.

--- MagicAveragePrice/Program.cs
+++ MagicAveragePrice/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
 
 namespace MagicAveragePrice
 {
@@ -10,6 +14,108 @@ namespace MagicAveragePrice
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Enter 3 character set code");
+                var setCode = Console.ReadLine().Trim();
+                if (setCode.Length > 3)
+                {
+                    Console.WriteLine("ERROR: set code is 3 characters long");
+                }
+                var obj = GetAllCards(setCode);
+
+                var Rarities = GetRarities(obj);
+
+                Console.WriteLine("Average prices for set:");
+                foreach (var rar in Rarities.Values)
+                {
+                    Console.WriteLine($"Rarity:{rar.RarityName}     No:{rar.TotalCards}");
+                    Console.WriteLine($"Total USD:{rar.TotalPriceUSD}   Average:{Math.Round(rar.TotalPriceUSD / rar.TotalCards, 2)}");
+                    Console.WriteLine($"Total EUR:{rar.TotalPriceEUR}   Average:{Math.Round(rar.TotalPriceEUR / rar.TotalCards, 2)}");
+                    Console.WriteLine($"Total TIX:{rar.TotalPriceTIX}   Average:{Math.Round(rar.TotalPriceTIX / rar.TotalCards, 2)}");
+
+                }
+
+            }
         }
+
+
+        public static Dictionary<string, Rarity> GetRarities(RootObject obj)
+        {
+            Dictionary<string, Rarity> Rarities = new Dictionary<string, Rarity>();
+
+            foreach (var datum in obj.data)
+            {
+                if (!Rarities.ContainsKey(datum.rarity))
+                {
+                    Rarities.Add(datum.rarity, new Rarity()
+                    {
+                        RarityName = datum.rarity,
+                        TotalCards = 0,
+                        TotalPriceEUR = 0,
+                        TotalPriceTIX = 0,
+                        TotalPriceUSD = 0
+                    });
+                }
+
+                try
+                {
+                    var rarity = Rarities[datum.rarity];
+
+                    rarity.TotalPriceEUR += Convert.ToDecimal(datum.prices.eur);
+                    rarity.TotalPriceUSD += Convert.ToDecimal(datum.prices.usd);
+                    rarity.TotalPriceTIX += Convert.ToDecimal(datum.prices.tix);
+                    rarity.TotalCards++;
+
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                    throw;
+                }
+            }
+            return Rarities; 
+        }
+
+
+
+        public static RootObject GetAllCards(string setName)
+        {
+            return GetRootObject($"https://api.scryfall.com/cards/search?q=s:{setName}"); 
+        }
+
+        public static RootObject GetRootObject(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var stream = new StreamReader(response.GetResponseStream());
+                var obj = JsonConvert.DeserializeObject<RootObject>(stream.ReadToEnd()); 
+                if (obj.has_more)
+                {
+                    var nextPage = GetRootObject(obj.next_page);
+                    obj.data.AddRange(nextPage.data); 
+                }
+                return obj; 
+            }
+            else
+                throw new Exception($"Error with conntecting:{response.StatusCode.ToString()}"); 
+        }
+    }
+    class Rarity
+    {
+        public string RarityName { get; set; }
+        public decimal TotalPriceUSD { get; set; }
+        public decimal TotalPriceEUR { get; set; }
+        /// <summary>
+        /// MTGO currency? 
+        /// </summary>
+        public decimal TotalPriceTIX { get; set; }
+        public int TotalCards { get; set; }
     }
 }
---
+++ MagicAveragePrice/SkryfallApi.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicAveragePrice
+{
+    public class ImageUris
+    {
+        public string small { get; set; }
+        public string normal { get; set; }
+        public string large { get; set; }
+        public string png { get; set; }
+        public string art_crop { get; set; }
+        public string border_crop { get; set; }
+    }
+
+    public class Legalities
+    {
+        public string standard { get; set; }
+        public string future { get; set; }
+        public string frontier { get; set; }
+        public string modern { get; set; }
+        public string legacy { get; set; }
+        public string pauper { get; set; }
+        public string vintage { get; set; }
+        public string penny { get; set; }
+        public string commander { get; set; }
+        public string duel { get; set; }
+        public string oldschool { get; set; }
+    }
+
+    public class Prices
+    {
+        public string usd { get; set; }
+        public string usd_foil { get; set; }
+        public string eur { get; set; }
+        public string tix { get; set; }
+    }
+
+    public class RelatedUris
+    {
+        public string gatherer { get; set; }
+        public string tcgplayer_decks { get; set; }
+        public string edhrec { get; set; }
+        public string mtgtop8 { get; set; }
+    }
+
+    public class PurchaseUris
+    {
+        public string tcgplayer { get; set; }
+        public string cardmarket { get; set; }
+        public string cardhoarder { get; set; }
+    }
+
+    public class AllPart
+    {
+        public string @object { get; set; }
+        public string id { get; set; }
+        public string component { get; set; }
+        public string name { get; set; }
+        public string type_line { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class Datum
+    {
+        public string @object { get; set; }
+        public string id { get; set; }
+        public string oracle_id { get; set; }
+        public List<int> multiverse_ids { get; set; }
+        public int mtgo_id { get; set; }
+        public int arena_id { get; set; }
+        public int tcgplayer_id { get; set; }
+        public string name { get; set; }
+        public string lang { get; set; }
+        public string released_at { get; set; }
+      //  public string uri { get; set; }
+        public string scryfall_uri { get; set; }
+        public string layout { get; set; }
+        public bool highres_image { get; set; }
+        public ImageUris image_uris { get; set; }
+        public string mana_cost { get; set; }
+        public float cmc { get; set; }
+        public string type_line { get; set; }
+        public string oracle_text { get; set; }
+        public List<object> colors { get; set; }
+        public List<object> color_identity { get; set; }
+        public Legalities legalities { get; set; }
+        public List<string> games { get; set; }
+        public bool reserved { get; set; }
+        public bool foil { get; set; }
+        public bool nonfoil { get; set; }
+        public bool oversized { get; set; }
+        public bool promo { get; set; }
+        public bool reprint { get; set; }
+        public bool variation { get; set; }
+        public string set { get; set; }
+       // public string set_name { get; set; }
+        public string set_type { get; set; }
+        public string set_uri { get; set; }
+        public string set_search_uri { get; set; }
+        public string scryfall_set_uri { get; set; }
+        public string rulings_uri { get; set; }
+        public string prints_search_uri { get; set; }
+        public string collector_number { get; set; }
+        public bool digital { get; set; }
+        public string rarity { get; set; }
+        public string flavor_text { get; set; }
+        public string illustration_id { get; set; }
+        public string card_back_id { get; set; }
+        public string artist { get; set; }
+        public string border_color { get; set; }
+        public string frame { get; set; }
+        public bool full_art { get; set; }
+        public bool textless { get; set; }
+        public bool booster { get; set; }
+        public bool story_spotlight { get; set; }
+        public int edhrec_rank { get; set; }
+        public Prices prices { get; set; }
+        public RelatedUris related_uris { get; set; }
+        public PurchaseUris purchase_uris { get; set; }
+        public string power { get; set; }
+        public string toughness { get; set; }
+        public List<string> promo_types { get; set; }
+        public string loyalty { get; set; }
+        public List<AllPart> all_parts { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string @object { get; set; }
+        public int total_cards { get; set; }
+        public bool has_more { get; set; }
+        public string next_page { get; set; }
+        public List<Datum> data { get; set; }
+    }
+}
